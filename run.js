0b33afeff92fd22e9a21afbaf0893980180d59c3
Norimitsu Yamashita <tugend.licht@gmail.com>
2019-02-07 18:29:15 +0900

    Remove opening browser scripts

--- run.js
+++ run.js
@@ -6,11 +6,11 @@ var shell = require("shelljs");
 var os = require('os');
 var path = require("path");
 if(os.platform() === "win32"){
-    shell.exec(`start http://localhost:8080 && node ${path.join(__dirname, "app.js")}`);
+    shell.exec(`node ${path.join(__dirname, "app.js")}`);
 }
 else if(os.platform() === "darwin"){
-    shell.exec(`open http://localhost:8080 && node ${path.join(__dirname, "app.js")}`);
+    shell.exec(`node ${path.join(__dirname, "app.js")}`);
 }
 else if(os.platform() === "linux"){
-    shell.exec(`xdg-open http://localhost:8080 && node ${path.join(__dirname, "app.js")}`);
+    shell.exec(`node ${path.join(__dirname, "app.js")}`);
 }
